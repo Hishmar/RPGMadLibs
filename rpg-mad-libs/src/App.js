de1af7269fed6beb88bcac96ba3434e8f9ef0de5
Matthew Larson <mllarson317@gmail.com>
2024-05-23 23:13:23 -0500

    finished and tested the s3 connector functions, needs error handling but moving to ui functions next

--- rpg-mad-libs/src/App.js
+++ rpg-mad-libs/src/App.js
@@ -1,17 +1,25 @@
 import './App.css';
-import {useEffect, useState} from 'react';
+import {useEffect, useState, useCallback} from 'react';
 import {getListOfLists, getList, putList} from './s3connector.js'
 
 function App() {
-  const [fullList, setFullList] = useState({})
+  const [fullList, setFullList] = useState({names:['1']})
   const [usedList, setUsedList] = useState("")
-  const [currentList, setCurentList] = useState("")
+  const [currentList, setCurentList] = useState("dBNames")
   const [listOfLists, setListOfLists] = useState(['list0'])
-  useEffect(()=>setListOfLists(getListOfLists()), [])
+  const fetchList = async () => setFullList(await getList(currentList))
+  
+  useEffect(()=>{async function fetchLists() {
+    setListOfLists(await getListOfLists())
+  }
+  fetchLists();
+    },[])
   return (
     <div className="App">
-      <p>{listOfLists[0]}
-      </p>
+      <p>{listOfLists[0]}</p>
+      <button onClick={fetchList}>getStuff</button>
+      <p>{fullList.names}</p>
+      <button onClick={()=>putList({listName:"dBNames",names:["ralph","ralph2"]})}>Add Ralph</button>
     </div>
   );
 }
--- rpg-mad-libs/src/s3connector.js
+++ rpg-mad-libs/src/s3connector.js
@@ -1,19 +1,27 @@
-
-
+/* Functions for using s3 for storage, forces names to be unique on a list*/
+import xMLParser from 'react-xml-parser'
 export const getListOfLists = async () => {
-    let response = await fetch("http://dndmadlibs.s3.amazonaws.com/?list-type=2");
+    let response = await fetch("http://dndmadlibs.s3.amazonaws.com/?list-type=2",{cache: "no-store"});
     let responseBody = await response.text();
-    let XMLParser = require('react-xml-parser');
-    let xml = new XMLParser().parseFromString(responseBody);
-    console.log(xml.getElementsByTagName('Key'));
-    return ['list1', 'list2']
-    
+    let xml = new xMLParser().parseFromString(responseBody);
+    let listOfLists = [];
+    xml.getElementsByTagName('Key').forEach((res)=>{
+        let trimmedName = res.value.replace(".json","");
+        listOfLists.push(trimmedName)}
+    )
+    console.log(listOfLists)
+    return listOfLists
 }
 
-export const getList = () => {
-
+export const getList = async (listName) => {
+    let response = await fetch("http://dndmadlibs.s3.amazonaws.com/" + listName + ".json",{cache: "no-store"});
+    return await response.json();
 }
 
-export const putList = () => {
-
+export const putList = async (localList) => {
+    let s3List  = await getList(localList.listName);
+    let fullArray = localList.names.concat(...s3List.names)
+    let combinedNames = [...new Set(fullArray)]
+    s3List.names = combinedNames
+    return await fetch("http://dndmadlibs.s3.amazonaws.com/"+ localList.listName + ".json",{method: "PUT", body:JSON.stringify(s3List)});
 }
