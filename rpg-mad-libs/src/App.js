84e46c67642b0df16524c2262c74e75a0a90a198
hishmar <mllarson317@gmail.com>
2024-09-14 09:23:52 -0500

    continued UI updates and another json file for test data

--- rpg-mad-libs/src/App.js
+++ rpg-mad-libs/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import {useEffect, useState, useCallback} from 'react';
-import {getListOfLists, getList, putList} from './s3connector.js'
+import {getListOfLists, getList, putList} from './s3connector.js';
+import Form from 'react-bootstrap/Form';
 
 function App() {
   const [fullList, setFullList] = useState({names:['1']})
   const [usedList, setUsedList] = useState("")
-  const [currentList, setCurentList] = useState("dBNames")
+  const [currentList, setCurrentList] = useState("dBNames")
   const [listOfLists, setListOfLists] = useState(['list0'])
   const fetchList = async () => setFullList(await getList(currentList))
   
@@ -14,12 +15,17 @@ function App() {
   }
   fetchLists();
     },[])
+
+  const listOptions = listOfLists.map((list)=>
+  <option value={list} key={list}>{list}</option>
+  )
   return (
     <div className="App">
-      <p>{listOfLists[0]}</p>
-      <button onClick={fetchList}>getStuff</button>
-      <p>{fullList.names}</p>
-      <button onClick={()=>putList({listName:"dBNames",names:["ralph","ralph2"]})}>Add Ralph</button>
+      <Form.Select onChange={(e)=>setCurrentList(e.target.value)}>
+        {listOptions}
+      </Form.Select>
+      <button onClick={()=>fetchList}>Get Name</button>
+      <button onClick={()=>putList({listName:{currentList},names:["ralph","ralph2"]})}>Add Ralph</button>
     </div>
   );
 }
